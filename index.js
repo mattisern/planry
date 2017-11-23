6890aae901906ed8d4416908d5c61c23cf82523a
stefankarlsson <karlssonstefan88@gmail.com>
2017-11-23 20:21:38 +0100

    create basic routing with uuid. Not persisting for now

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+const uuid = require('uuid/v4');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/committest', (req, res) => res.send('hej'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+let app = express();
 
+//config/setup
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+//routes TODO:extract
+app.get('/', (req, res) => res.redirect('boards'))
+
+app.get('/boards', (req, res) => {
+	let newUuid = uuid();
+	//TODO: here we should actually create a board and persist it to the db. This just proves the point for now
+	res.redirect('boards/' + newUuid)
+});
+
+app.get('/boards/:uuid', (req, res) => {
+	//TODO: here we should start out by checking if we have a board in the db, otherwise someone is playing with our url and should just receive a 404
+	res.render('pages/boards', { 'uuid' : req.params.uuid })
+});
+
+
+//start our listener
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
