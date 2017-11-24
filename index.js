222c897d1914f72aa4fca7d8a4e5bfabdd269564
stefankarlsson <karlssonstefan88@gmail.com>
2017-11-25 00:01:42 +0100

    remove redundant files, editable name placeholder and styling

--- index.js
+++ index.js
@@ -11,19 +11,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//db
-app.get('/db', function (request, response) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.render('pages/db', {results: result.rows} ); }
-    });
-  });
-});
-
 //routes TODO:extract
 app.get('/', (req, res) => res.redirect('boards'))
 
