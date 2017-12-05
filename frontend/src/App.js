d94030e326665e2e0683f0f08f9a1b2d508df83e
Johan Oliwsson <johan@oliwsson.com>
2017-12-05 23:55:16 +0100

    Move css to style folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import {observer} from 'mobx-react';
 
-import "./bootstrap.min.css";
-import "./App.css";
+import "./style/bootstrap.min.css";
+import "./style/App.css";
 
 import Board from "./views/boards/Board";
 
