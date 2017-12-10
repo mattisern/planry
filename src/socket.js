52ee0fb3656092e23c87ecd11b4cbf67a1212c63
stefankarlsson <karlssonstefan88@gmail.com>
2017-12-10 16:25:13 +0100

    fix socket connect url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
-const socket = (id) => io.connect("", {query: "room=" + id});
+const socket = (id) => io.connect(process.env.REACT_APP_API, {query: "room=" + id});
 
 export default socket;
