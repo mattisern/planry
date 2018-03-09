d531f3b7e5d2578a5ad344755e14f82be9b829d9
stefankarlsson <9807413+StefanKarlsson@users.noreply.github.com>
2018-03-10 00:24:14 +0100

    fix indenting in welcomeWidget

--- app/welcomeWidget.js
+++ app/welcomeWidget.js
@@ -1,90 +1,90 @@
 module.exports = {
-        "name": "This is Planry",
-        "text": "Welcome to Planry!\n\nPlanry is a tool that helps you and your friends collaborate.\n\nStart by changing this text and send this URL to someone you want to share the board with.\nAdd new cards by pressing the placeholders, for instance \"+ Text\".\nYou can share this board by simple sharing the url\nAnd of course, you can change this text :)",
-        "richText": {
-            "entityMap": {},
-            "blocks": [
-            {
-                "key": "bt4rp",
-                "text": "Welcome to Planry!",
-                "type": "header-three",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "8r6mu",
-                "text": "",
-                "type": "unstyled",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "84p21",
-                "text": "Planry is a tool that helps you and your friends collaborate.",
-                "type": "unstyled",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "dp142",
-                "text": "",
-                "type": "unstyled",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "4j3l1",
-                "text": "Start by changing this text. Then send the URL of this board to someone you want to share it with :)",
-                "type": "unstyled",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "dojco",
-                "text": "You can also ...",
-                "type": "unstyled",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "20rnm",
-                "text": "Add new cards to your board.",
-                "type": "unordered-list-item",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "bnpaj",
-                "text": "Style your text with Markdown. Try making something bold by writing it in **double stars**.",
-                "type": "unordered-list-item",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            },
-            {
-                "key": "2i6tl",
-                "text": "And of course, you can change the name of your project, too! Just click the name and write whatever you want.",
-                "type": "unordered-list-item",
-                "depth": 0,
-                "inlineStyleRanges": [],
-                "entityRanges": [],
-                "data": {}
-            }
-            ]
+    "name": "This is Planry",
+    "text": "Welcome to Planry!\n\nPlanry is a tool that helps you and your friends collaborate.\n\nStart by changing this text and send this URL to someone you want to share the board with.\nAdd new cards by pressing the placeholders, for instance \"+ Text\".\nYou can share this board by simple sharing the url\nAnd of course, you can change this text :)",
+    "richText": {
+        "entityMap": {},
+        "blocks": [
+        {
+            "key": "bt4rp",
+            "text": "Welcome to Planry!",
+            "type": "header-three",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "8r6mu",
+            "text": "",
+            "type": "unstyled",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "84p21",
+            "text": "Planry is a tool that helps you and your friends collaborate.",
+            "type": "unstyled",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "dp142",
+            "text": "",
+            "type": "unstyled",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "4j3l1",
+            "text": "Start by changing this text. Then send the URL of this board to someone you want to share it with :)",
+            "type": "unstyled",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "dojco",
+            "text": "You can also ...",
+            "type": "unstyled",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "20rnm",
+            "text": "Add new cards to your board.",
+            "type": "unordered-list-item",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "bnpaj",
+            "text": "Style your text with Markdown. Try making something bold by writing it in **double stars**.",
+            "type": "unordered-list-item",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
+        },
+        {
+            "key": "2i6tl",
+            "text": "And of course, you can change the name of your project, too! Just click the name and write whatever you want.",
+            "type": "unordered-list-item",
+            "depth": 0,
+            "inlineStyleRanges": [],
+            "entityRanges": [],
+            "data": {}
         }
+        ]
     }
+}
