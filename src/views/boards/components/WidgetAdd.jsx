cb7ac844685a720cf29d76fbb36ecd9533e016ef
Johan Oliwsson <johan@oliwsson.com>
2018-05-10 18:23:03 +0200

    Update add card functionality

--- src/views/boards/components/WidgetAdd.jsx
+++ src/views/boards/components/WidgetAdd.jsx
@@ -5,11 +5,15 @@ const WidgetAdd = observer(class WidgetAdd extends React.Component {
     render() {
         return (
             <div className="add-widget-card">
-                <div className="clickable widget-add-button" onClick={() => this.props.board.addWidget("checklist")}>
-                  <span>
-                    <img className="add-widget-plus" src="/img/plus.png" /><br />
-                    ADD CARD
-                  </span>
+                <h3 className="add-card">ADD CARD</h3>
+                <div className="add-widget-buttons">
+                    <div className="clickable widget-add-button" onClick={() => this.props.board.addWidget("text")}>
+                        <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+                        <img className="add-widget-plus" src="/img/widget_text.png" /><br />
+                    </div>
+                    <div className="clickable widget-add-button" onClick={() => this.props.board.addWidget("checklist")}>
+                        <img className="add-widget-plus" src="/img/widget_checklist.png" /><br />  
+                    </div>
                 </div>
             </div>
         );
